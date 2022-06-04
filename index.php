9ffb4e1ff89ba9fd14350c9e7ba3248ab13f27e0
Daniel Garzon <daniel.garzon@sercop.gob.ec>
2022-06-04 08:22:04 -0500

    actualizacion de blog y estilos

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html>
+
     <head>
         <title>
             AQUÍ VA UN POST
@@ -6,9 +7,13 @@
         <link rel="stylesheet" href="css/estilos.css">
     </head>
     <body>
-        <h1>Aquí vamos hacer un blog post</h1>
-        <p>Párrafo de inicio</p>
+        <div id="container">
+            <div id="post">
+                <h1>TITULO NUEVO PARA EL POST</h1>
+                <p>Fragmento de un escrito con unidad temática, que queda diferenciado del resto de fragmentos por un punto y aparte y generalmente también por llevar letra mayúscula inicial y un espacio en blanco en el margen izquierdo de alineación del texto principal de la primera línea.</p>
+            </div>
+        </div>
     </body>
 
-</html>
+    </html>
 

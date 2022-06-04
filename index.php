7fd9041d0aea7e9e315d63f8cd19260864ebcbb0
Daniel Garzon <daniel.garzon@sercop.gob.ec>
2022-06-04 08:55:49 -0500

    nuevo parrafo y cambio de tipografia

--- index.php
+++ index.php
@@ -10,7 +10,12 @@
         <div id="container">
             <div id="post">
                 <h1>TITULO NUEVO PARA EL POST</h1>
-                <p>Fragmento de un escrito con unidad temática, que queda diferenciado del resto de fragmentos por un punto y aparte y generalmente también por llevar letra mayúscula inicial y un espacio en blanco en el margen izquierdo de alineación del texto principal de la primera línea.</p>
+                <p>
+                    Fragmento de un escrito con unidad temática, que queda diferenciado del resto de fragmentos por un punto y aparte y generalmente también por llevar letra mayúscula inicial y un espacio en blanco en el margen izquierdo de alineación del texto principal de la primera línea.
+                </p>
+                <p>
+                    Un párrafo, también llamado parágrafo, es una unidad comunicativa formada por un conjunto de oraciones secuenciales que trata un mismo tema. Está compuesto por un conjunto de oraciones que tienen cierta unidad temática o que, sin tenerla, se enuncian juntas.
+                </p>
             </div>
         </div>
     </body>

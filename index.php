36d6171eaaf425392124ddb3e97397ca5a5c2916
Daniel Garzon <daniel.garzon@sercop.gob.ec>
2022-06-04 07:53:18 -0500

    modificacion de codigo html - v1

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <><title>
+            AQUÍ VA UN POST
+        </title>
+        <link rel="stylesheet" href="css/estilos.css">
+    </head>
+    <body>
+        <h1>Aquí vamos hacer un blog post</h1>
+        <p>Párrafo de inicio</p>
+    </body>
+
+</html>
+

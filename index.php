3db49ae99a94a767ae64bb4965c1b959b47a1d39
Daniel Garzon <daniel.garzon@sercop.gob.ec>
2022-06-04 08:37:37 -0500

    Creacion de CABECERA

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     </head>
     <body>
         <div id="container">
+            <div id= "cabecera">
+                CABECERA
+            </div>
             <div id="post">
                 <h1>TITULO NUEVO PARA EL POST</h1>
                 <p>Fragmento de un escrito con unidad temática, que queda diferenciado del resto de fragmentos por un punto y aparte y generalmente también por llevar letra mayúscula inicial y un espacio en blanco en el margen izquierdo de alineación del texto principal de la primera línea.</p>

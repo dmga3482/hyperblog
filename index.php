87433800e7136490cf0ac868a125b026d976b3cd
Daniel Garzon <dmga3482@yahoo.com>
2022-06-05 17:12:43 -0500

    carga de metodo HTML para tiñdes

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
             <div id="post">
                 <h1>TITULO NUEVO PARA EL POST - CAMBIO DESDE GITHUB</h1>
                 <p>
-                    Fragmento de un escrito con unidad temática, que queda diferenciado del resto de fragmentos por un punto y aparte y generalmente también por llevar letra mayúscula inicial y un espacio en blanco en el margen izquierdo de alineación del texto principal de la primera línea.
+                    Fragmento de un escrito con unidad tem&aacutetica, que queda diferenciado del resto de fragmentos por un punto y aparte y generalmente también por llevar letra mayúscula inicial y un espacio en blanco en el margen izquierdo de alineación del texto principal de la primera línea.
                 </p>
                 <p>
-                    Un párrafo, también llamado parágrafo, es una unidad comunicativa formada por un conjunto de oraciones secuenciales que trata un mismo tema. Está compuesto por un conjunto de oraciones que tienen cierta unidad temática o que, sin tenerla, se enuncian juntas.
+                    Un p&aacuterrafo, tambi&eacuten llamado par&aacutegrafo, es una unidad comunicativa formada por un conjunto de oraciones secuenciales que trata un mismo tema. Está compuesto por un conjunto de oraciones que tienen cierta unidad temática o que, sin tenerla, se enuncian juntas.
                 </p>
                 <p>
                     <b>Párrafo para fix_type.</b>
